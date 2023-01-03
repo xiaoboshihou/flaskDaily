3bcab9f446c32d60c512273c5b00e15ffdebfa6f
lixueyuan <lixueyuan@wondersgroup.com>
2023-01-03 17:33:00 +0800

    'flask脚本'

--- manage.py
+++ manage.py
@@ -9,6 +9,17 @@ def hello():
     print('hello world')
 
 
+@manager.command
+def demo():
+    print('无参命令')
+
+
+@manager.option("-u", "--username", dest="username")
+@manager.option("-p", "--password", dest="password")
+def login(username, password):
+    print("用户名:{}  密码: {}".format(username, password))
+
+
 if __name__ == '__main__':
     manager.run()
 
